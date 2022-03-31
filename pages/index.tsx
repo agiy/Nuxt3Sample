064ef20745b0ebdcaa25f605e46ebf5156e0f04a
agiy <agiyagiyagiy@gmail.com>
2022-03-31 22:07:36 +0900

    [update] Vuetify導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
+// @ts-ignore
+import {VBtn} from "vuetify/lib/components";
 
 export default defineComponent({
-  render () {
-    return <h1>Index page gogo</h1>
+  render() {
+    return (
+      <div>
+        <h1>Index page gogo</h1>
+
+        <VBtn>fdafafdafsfa</VBtn>
+      </div>
+    )
   }
 })
