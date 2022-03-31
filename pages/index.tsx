52641d8102ad71a230fbec96dac54876c50520a9
agiy <agiyagiyagiy@gmail.com>
2022-03-31 21:28:45 +0900

    [add] サンプルページ作成

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  render () {
+    return <h1>Index page gogo</h1>
+  }
+})
---
+++ pages/users/index.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  render () {
+    return <h1>users Index page gogo</h1>
+  }
+})
